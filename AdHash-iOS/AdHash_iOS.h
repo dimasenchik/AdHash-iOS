800fe379f8c0252b539b03aa4a273700441e1830
Senchik Dima <dmitriy.senchik@gmail.com>
2019-10-13 13:21:36 +0300

    Initial Commit

---
+++ AdHash-iOS/AdHash_iOS.h
@@ -0,0 +1,19 @@
+//
+//  AdHash_iOS.h
+//  AdHash-iOS
+//
+//  Created by Dima Senchik on 10/13/19.
+//  Copyright © 2019 Dima Senchik. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AdHash_iOS.
+FOUNDATION_EXPORT double AdHash_iOSVersionNumber;
+
+//! Project version string for AdHash_iOS.
+FOUNDATION_EXPORT const unsigned char AdHash_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdHash_iOS/PublicHeader.h>
+
+
